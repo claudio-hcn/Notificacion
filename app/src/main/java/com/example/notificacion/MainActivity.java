1dd41db4ded16f0abd93156f87d38410daa8a7b4
claudio-hcn <claudio.hcn@outlook.com>
2021-11-19 18:25:47 -0300

    Se agrega compatibilidad para versiones sobre api 26

--- app/src/main/java/com/example/notificacion/MainActivity.java
+++ app/src/main/java/com/example/notificacion/MainActivity.java
@@ -8,7 +8,9 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -18,51 +20,77 @@ import android.widget.QuickContactBadge;
 
 public class MainActivity extends AppCompatActivity {
     private Button btNotificacion;
+    private Button btnActualizar;
     private PendingIntent pendingIntent;
-    private final static String CHANNEL_ID="NOTIFICACION";
-    private final static int NOTIFICACION_ID=0;
+    private final static String CHANNEL_ID = "NOTIFICACION";
+    private final static int NOTIFICACION_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btNotificacion=findViewById(R.id.btNotificacion);
+        btNotificacion = findViewById(R.id.btNotificacion);
         btNotificacion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                createNotificacion();
+                armarNotificacion();
+            }
+        });
+        btnActualizar = findViewById(R.id.btnActualizar);
+        btnActualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actualizarNoti();
             }
         });
     }
 
-    private void createNotificacion(){
-        NotificationCompat.Builder builder= new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID);
+    private void armarNotificacion() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID);
         Context contexto = getApplicationContext();
         NotificationManager notificador = (NotificationManager) getSystemService(contexto.NOTIFICATION_SERVICE);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            String canalID="2";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String canalID = "2";
             String canalNombre = "Mensajes";
             String canalDescribe = "Canal de mensajes";
             int importancia = NotificationManager.IMPORTANCE_HIGH;
             NotificationChannel miCanal = new NotificationChannel(canalID, canalNombre, importancia);
             miCanal.setDescription(canalDescribe);
-            miCanal.enableLights(true);
-            miCanal.setLightColor(Color.BLUE); // Esto no lo soportan todos los dispositivos
-            miCanal.enableVibration(true);
             notificador.createNotificationChannel(miCanal);
             builder = new NotificationCompat.Builder(contexto, canalID);
         }
         builder.setSmallIcon(R.drawable.ic_baseline_add_alert_24);
-        builder.setContentTitle("notificacion androi");
-        builder.setContentText("wena conmpadre");
+        builder.setContentTitle("notificacion android");
+        builder.setContentText("wena compadre");
         builder.setColor(Color.RED);
         builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
-        builder.setLights(Color.MAGENTA,1000,1000);
-        builder.setVibrate(new long[]{2000,1000,1000,1000,1000});
+        builder.setLights(Color.MAGENTA, 1000, 1000);
+        builder.setVibrate(new long[]{2000, 1000, 1000, 1000, 1000});
         builder.setDefaults(Notification.DEFAULT_SOUND);
+        builder.setStyle(new NotificationCompat.BigTextStyle().bigText(getString(R.string.mnsj_largo)));
 
-        NotificationManagerCompat notificationManagerCompat=NotificationManagerCompat.from(getApplicationContext());
-        notificationManagerCompat.notify(NOTIFICACION_ID,builder.build());
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
+        notificationManagerCompat.notify(NOTIFICACION_ID, builder.build());
 
     }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+    private void actualizarNoti(){
+
+    }
+
 }
